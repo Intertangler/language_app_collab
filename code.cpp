871c8d191d012cb21bff0e7e21f5b5228cbe26fe
Intertangler <ian.hoffecker>
2017-12-10 14:01:36 +0100

    added a class to code.cpp

--- code.cpp
+++ code.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+class nameRecordClass{
+	public:
+		void setName(string x){
+		name = x;
+		}
+		string getName(){
+			return name;
+		}
+	private:
+		string name;
+};
 
 int main() {
+	nameRecordClass someObject;
+	someObject.setName("asdf");
+	cout << someObject.getName() << endl;
 	cout << "Code and stuff" << endl;
 	return 0;
 }
